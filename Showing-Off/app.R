57d21d2812ee42b7c8d2f4c41b080cdb30f950fe
Anan Hafez <ananhafez@college.harvard.edu>
2019-10-22 12:27:18 -0400

    Update

--- Showing-Off/app.R
+++ Showing-Off/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("How many times will this geyser blow up?"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
